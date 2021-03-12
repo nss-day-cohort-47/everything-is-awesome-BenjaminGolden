1ddd3e7c0972abcded38616f5a16b2966a6f7080
BenjaminGolden <b.j.golden@outlook.com>
2021-03-12 11:17:24 -0600

    names to upper case

--- scripts/legos/LegoDetail.js
+++ scripts/legos/LegoDetail.js
@@ -3,13 +3,14 @@ import { invertColor } from './../helpers.js';
 export const LegoDetail = (brick) => {
 
   let block = `<section class="block-wrapper" style="background-color:#${brick.ColorHex}">
-              <h3>Name: ${brick.LegoName}</h3>
+              <h3>Name: ${brick.LegoName.toUpperCase()}</h3>
               <div class="block-years">Manufactured: ${brick.YearFrom} - ${brick.YearTo}</div>             
               `;
     const notes = brick.Notes;
     if (notes) {
       block += `<div class="lego__notes">Notes: ${notes}</div> `;
     }
+    block += '</section>'
   const link = brick.ColorstreamLinkImage;
   if (link) {
     //true? wrap the block in an a tag
