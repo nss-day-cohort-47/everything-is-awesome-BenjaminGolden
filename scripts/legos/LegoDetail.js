bd051d284a472ad349520fdc2dc7d73aeaba7575
BenjaminGolden <b.j.golden@outlook.com>
2021-03-12 10:44:17 -0600

    buttons and notes added

--- scripts/legos/LegoDetail.js
+++ scripts/legos/LegoDetail.js
@@ -4,8 +4,12 @@ export const LegoDetail = (brick) => {
 
   let block = `<section class="block-wrapper" style="background-color:#${brick.ColorHex}">
               <h3>Name: ${brick.LegoName}</h3>
-              <div class="block-years">Manufactured ${brick.YearFrom} - ${brick.YearTo}</div>
+              <div class="block-years">Manufactured: ${brick.YearFrom} - ${brick.YearTo}</div>             
               `;
+    const notes = brick.Notes;
+    if (notes) {
+      block += `<div class="lego__notes">Notes: ${notes}</div> `;
+    }
   const link = brick.ColorstreamLinkImage;
   if (link) {
     //true? wrap the block in an a tag
